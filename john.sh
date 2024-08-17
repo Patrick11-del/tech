1bf4fab0daece62c731f5592cce0ccb71f883653
Christian Patrick <christianpatrick1414@gmail.com>
2024-08-17 21:01:40 +0100

    pmp file

---
+++ john.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+num1=$1
+num2=$2
+num3=$3
+echo "the sum of $num1 + $num + $num3 is $((( num1 + num2 + num3)))"#!/bin/bash
+num1=$1
+num2=$2
+num3=$3
+echo "the sum of $num1 + $num + $num3 is $((( num1 + num2 + nu`:
